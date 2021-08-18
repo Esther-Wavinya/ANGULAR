8fb9106ca3a3cb33821db78db3a5f4ad27197d98
Esther Wavinya <estherwavinya80@gmail.com>
2021-08-18 10:12:08 +0300

    customizing card component from parent component

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  posts = [
+    { 
+      title: "Neat Tree",
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today' 
+    },
+    { 
+      title: "Snowy mountain",
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountain lava',
+      content: 'Here is a picture of a snowy mountain' 
+    },
+    { 
+      title: "Mountain biking",
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking12222',
+      content: 'I did some biking today' 
+    }
   
+
+  ]
 }
